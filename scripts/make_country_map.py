e33b206370443c7eb46d2315dd69553f1042fd7b
Barney Potter <barney.potter.24@gmail.com>
2020-04-27 15:38:10 +0200

    Add country-region maps

---
+++ scripts/make_country_map.py
@@ -0,0 +1,30 @@
+'''make_country_map.py
+'''
+import sys, os
+
+def main():
+    input_file = "../phylogeography/assets/city_country_full.csv"
+    output_file = "../phylogeography/assets/country_region_partial.tsv"
+
+
+    countries_seen = set()
+
+    with open(input_file,'r') as i:
+        with open(output_file, 'w') as o:
+            header = i.readline()
+            o.write("country\tcontinent\n")
+            for line in i.readlines():
+                line = line.split(',')
+                continent = line[0].lower().capitalize()
+                country = line[2].lower().capitalize()
+                if country not in countries_seen:
+                    newline = f"{country}\t{continent}\n"
+                    countries_seen.add(country)
+                    o.write(newline)
+
+
+
+
+
+if __name__ == '__main__':
+    main()
